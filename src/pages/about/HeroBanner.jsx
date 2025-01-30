5cc016e28f4bc6c9cb08ce50d804bbc90497cdd7
Danish <ammarsaifi5376@gmail.com>
2025-01-30 16:17:14 +0530

    Add Visionary Banner image for About page hero section

--- src/pages/about/HeroBanner.jsx
+++ src/pages/about/HeroBanner.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
-import img from "../../assets/about/Banner-Image.webp";
+import img from "../../assets/about/Visionary-Banner.webp";
 const HeroBanner = () => {
   return (
     <motion.div
