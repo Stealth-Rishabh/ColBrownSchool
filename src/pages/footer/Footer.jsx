6d2a201c189422b6797aa3f11f8276f0e5c918f8
Danish <ammarsaifi5376@gmail.com>
2025-02-07 19:59:12 +0530

    Update Footer Calendar Link and Add Security Attributes to External Links

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -32,7 +32,7 @@ export default function Footer() {
       { name: "Newsletter", href: "#" },
       {
         name: "Calendar",
-        href: "/src/assets/calendar-january-may-2024.pdf",
+        href: "https://col-brown-school.vercel.app/assets/calendar-january-may-2024-Dre_knEo.pdf",
         target: 1,
       },
       { name: "Notice(s)", href: "/notices" },
